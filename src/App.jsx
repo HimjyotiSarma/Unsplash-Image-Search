53a78f4af8e219af1300c38143a5c851098c08da
HimjyotiSarma <127434307+HimjyotiSarma@users.noreply.github.com>
2023-12-09 13:57:22 +0530

    Pagination naming update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Gallery from './Gallery'
 import SearchForm from './SearchForm'
 import ThemeToggle from './ThemeToggle'
-import Pagination from './pagination'
+import Pagination from './Pagination'
 
 const App = () => {
   return (
